214355571ba4cb748271247cb722994fc5a21e40
marwan <marwan.elansarey@gmail.com>
2023-04-28 23:25:41 +0200

    some of const values will use

---
+++ lib/core/constants.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+const backgroundApp = Color(0xff100B20);
+
+const transtionToScreen = Duration(seconds: 4);
+
+const familyGTSectraFineRegular = 'GT Sectra Fine Regular';
+
+const colorStar = Color(0xffFFDD4F);
+
+const colorGrey = Color(0xff707070);
+
+const imgNotAvalible =
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNT0xwyLstvC7wH8jYIKur3GTcSq-g6fj2EbL4wk-qaONHYjBswa3rpFsZJeEjuXcG-lw&usqp=CAU";
