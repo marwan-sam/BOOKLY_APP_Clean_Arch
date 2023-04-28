ad3badec4e2af59d1c184a0c7e2dfc03dc95264e
marwan <marwan.elansarey@gmail.com>
2023-04-28 23:32:02 +0200

    model book create

---
+++ lib/Features/Home/domain/data/models/books_model/books_model.dart
@@ -0,0 +1,82 @@
+import 'package:bookly_app/Features/Home/domain/entities/book_entity.dart';
+import 'package:bookly_app/core/constants.dart';
+import 'package:equatable/equatable.dart';
+
+import 'access_info.dart';
+import 'sale_info.dart';
+import 'search_info.dart';
+import 'volume_info.dart';
+
+class BooksModel extends BookEntity {
+  final String? kind;
+  final String? id;
+  final String? etag;
+  final String? selfLink;
+  final VolumeInfo volumeInfo;
+  final SaleInfo? saleInfo;
+  final AccessInfo? accessInfo;
+  final SearchInfo? searchInfo;
+
+  BooksModel({
+    this.kind,
+    this.id,
+    this.etag,
+    this.selfLink,
+    required this.volumeInfo,
+    this.saleInfo,
+    this.accessInfo,
+    this.searchInfo,
+  }) : super(
+          idBook: id!,
+          title: volumeInfo.title!,
+          image: volumeInfo.imageLinks?.thumbnail ??
+              volumeInfo.imageLinks?.smallThumbnail ??
+              imgNotAvalible,
+          authorName: volumeInfo.authors?.first ?? 'No Name',
+          price: 0.0,
+          rating: volumeInfo.averageRating,
+        );
+
+  factory BooksModel.fromJson(Map<String, dynamic> json) => BooksModel(
+        kind: json['kind'] as String?,
+        id: json['id'] as String?,
+        etag: json['etag'] as String?,
+        selfLink: json['selfLink'] as String?,
+        volumeInfo:
+            VolumeInfo.fromJson(json['volumeInfo'] as Map<String, dynamic>),
+        saleInfo: json['saleInfo'] == null
+            ? null
+            : SaleInfo.fromJson(json['saleInfo'] as Map<String, dynamic>),
+        accessInfo: json['accessInfo'] == null
+            ? null
+            : AccessInfo.fromJson(json['accessInfo'] as Map<String, dynamic>),
+        searchInfo: json['searchInfo'] == null
+            ? null
+            : SearchInfo.fromJson(json['searchInfo'] as Map<String, dynamic>),
+      );
+
+  Map<String, dynamic> toJson() => {
+        'kind': kind,
+        'id': id,
+        'etag': etag,
+        'selfLink': selfLink,
+        'volumeInfo': volumeInfo.toJson(),
+        'saleInfo': saleInfo?.toJson(),
+        'accessInfo': accessInfo?.toJson(),
+        'searchInfo': searchInfo?.toJson(),
+      };
+
+  @override
+  List<Object?> get props {
+    return [
+      kind,
+      id,
+      etag,
+      selfLink,
+      volumeInfo,
+      saleInfo,
+      accessInfo,
+      searchInfo,
+    ];
+  }
+}
