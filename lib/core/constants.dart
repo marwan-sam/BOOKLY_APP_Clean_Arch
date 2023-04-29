89d2e4e9f594be7f445678c6e2669c83e0ff8d98
marwan <marwan.elansarey@gmail.com>
2023-04-29 17:06:46 +0200

    hive feature box

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -2,6 +2,12 @@ import 'package:flutter/material.dart';
 
 const backgroundApp = Color(0xff100B20);
 
+const backgroundAppBarF = Color(0xff403c4d);
+
+const colorAppBarNotSelected = Color(0xffa8a6ae);
+
+const colorAppBarSelected = Color(0xffffffff);
+
 const transtionToScreen = Duration(seconds: 4);
 
 const familyGTSectraFineRegular = 'GT Sectra Fine Regular';
@@ -12,3 +18,5 @@ const colorGrey = Color(0xff707070);
 
 const imgNotAvalible =
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNT0xwyLstvC7wH8jYIKur3GTcSq-g6fj2EbL4wk-qaONHYjBswa3rpFsZJeEjuXcG-lw&usqp=CAU";
+
+const keyFeatureBoxHive = "Hive_feature_box";
