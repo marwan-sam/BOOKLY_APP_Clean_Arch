db41c7899f8c0ccc852c17d465541b15cc272f7e
marwan <marwan.elansarey@gmail.com>
2023-05-03 21:46:03 +0200

    change ui bottom bar

--- lib/Features/Home/presentation/screens/widget/bottom_bar_floating.dart
+++ lib/Features/Home/presentation/screens/widget/bottom_bar_floating.dart
@@ -18,22 +18,25 @@ class BottomBarFloat extends StatefulWidget {
 class _BottomBarFloatState extends State<BottomBarFloat> {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(right: 20, left: 20, bottom: 20),
-      child: BottomBarFloating(
+    return SizedBox(
+      height: 60,
+      child: Padding(
+        padding: const EdgeInsets.only(right: 20, left: 20, top: 0, bottom: 7),
+        child: BottomBarFloating(
           borderRadius: BorderRadius.circular(30),
           blur: 5,
           backgroundColor: backgroundAppBarF,
           color: colorAppBarNotSelected,
           colorSelected: colorAppBarSelected,
           items: widget.items,
-          paddingVertical: 20,
           indexSelected: widget.currentIndex,
           onTap: (int index) {
             setState(() {
               widget.currentIndex = index;
             });
-          }),
+          },
+        ),
+      ),
     );
   }
 }
